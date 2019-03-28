ada971016fc61360f17eadd57c4be1579f521046
Jennifer Ukegbu <jukegbu1@gmail.com>
2019-03-28 14:22:46 -0400

    timesheet class formulas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,39 +1,82 @@
-package com.company;
+//package com.company;
+//
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args){
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        int hoursWorked = keyboard.nextInt();
+//
+//        double payRate = 45;
+//        int otHours = hoursWorked - 40;
+//
+//
+//        //employees work more than 40hrs
+//            if (hoursWorked > 40){
+//            double otPay = (otHours) * (payRate * 1.5);
+//            double wages = (otPay) + (40 * payRate);
+//
+//            //employees who work no overtime
+//        } else if (hoursWorked <= 40){
+//            double wkPay = (hoursWorked) * (payRate);
+//        }
+//
+//            //calculation for sick leave
+//        double sickLeave = 0;
+//        double totalHours = (hoursWorked + otHours);
+//
+//        if (totalHours >= 100)
+//        {sickLeave = (sickLeave +2.5);}
+//
+//        if (totalHours >= 300)
+//        {sickLeave = (sickLeave +4.5);}
+//
+//        if (totalHours >= 500)
+//        {sickLeave = (sickLeave +5);}
 
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args){
-        Scanner keyboard = new Scanner(System.in);
+        //In timesheet class
+        total=0;
 
-        int hoursWorked = keyboard.nextInt();
+        public int findtotal(timesheet){
+            for(i: timesheet.ts){
+                total = total +tstimes.hoursworked;
+            }
 
-        double payRate = 45;
-        int otHours = hoursWorked - 40;
+timesheet.setTotal(total);
 
-
-        //employees work more than 40hrs
-            if (hoursWorked > 40){
-            double otPay = (otHours) * (payRate * 1.5);
-            double wages = (otPay) + (40 * payRate);
-
-            //employees who work no overtime
-        } else if (hoursWorked <= 40){
-            double wkPay = (hoursWorked) * (payRate);
         }
 
-            //calculation for sick leave
-        double sickLeave = 0;
-        double totalHours = (hoursWorked + otHours);
 
-        if (totalHours >= 100)
-        {sickLeave = (sickLeave +2.5);}
 
-        if (totalHours >= 300)
-        {sickLeave = (sickLeave +4.5);}
 
-        if (totalHours >= 500)
-        {sickLeave = (sickLeave +5);}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
